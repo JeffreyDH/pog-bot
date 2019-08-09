20e7b6a1d2d0181da13a1b4e225d539d9feba5a5
Jeffrey Hernandez <jeffredh@usc.edu>
2019-08-09 00:02:05 -0700

    removed node_modules

--- commands/jeff.js
+++ commands/jeff.js
@@ -1,6 +1,7 @@
 module.exports = {
     name:'jeff',
     description: 'Greet HIM',
+    aliases:['creator','him'],
     execute(message, args){
         message.channel.send(`Hello, Creator. Welcome back to the incubator.`)
     }
--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const { prefix } =  require('./config.json');
 dotenv.config();
-client.once('ready', ()=>{
-    console.log('ready');
-});
+
 const commandFiles = fs.readdirSync('./commands/').filter(file=>file.endsWith('.js'));
+// register all of the commands
 for(const file of commandFiles)
 {
     const command = require(`./commands/${file}`);
@@ -16,19 +15,26 @@ for(const file of commandFiles)
 }
 client.on('message', (message)=>{
     if(!message.content.startsWith(`${prefix}`) || message.author.bot) return;
+    
     const args = message.content.slice(prefix.length).split('/ +/');
     const commandName = args.shift().toLowerCase();
     
-    if(!client.commands.has(commandName)) return;
+    const command = client.commands.get(commandName) ||
+        client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
     
-    const command = client.commands.get(commandName);
+        if(!command) return;
     
     if(command.args && !args.length){
-        return message.channel.send(`You didn't provide any arguments, ${message.author}`);
+        let reply = `You didn't provide any arguments, ${message.author}`;
+        if(command.usage){
+            reply += `\n The proper usage would be: \` ${prefix}${command.name} ${command.usage}\``;
+        }
+        return message.channel.send(reply);
     }
     if(command.guildOnly && message.channel.type !== 'text'){
         return message.reply('I can\'t execute that command inside DMs');
     }
+    
     try{
         command.execute(message, args);
     }
@@ -38,8 +44,4 @@ client.on('message', (message)=>{
     }
 });
 
-function respond(message, response)
-{
-    message.channel.send(response);
-}
 client.login(`${process.env.AUTH_TOKEN}`);
